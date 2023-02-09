30abebd4d7dadac1f901e2a398a00a8694d9cbb2
student@student.com <ti@admin.com>
2023-02-10 01:17:59 +0200

    final commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,84 @@
 
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import './App.css';
 import ExchangeBlock from './components/ExchangeBlock';
 
 function App() {
+  const ratesRef = useRef({})
   const [fromCurrency, setFromCurrency] = useState('UAH')
+  const [toCurrency, setToCurrency] = useState('USD')
+  const [fromPrice, setFromPrice] = useState(0)
+  const [toPrice, setToPrice] = useState(1)
+
+  useEffect(() => {
+    fetch('https://v6.exchangerate-api.com/v6/bada389b22f98c36c86cc6bb/latest/USD')
+    .then(res => res.json())
+    .then(json => {
+      ratesRef.current = json.conversion_rates
+      onChangeToPrice(1)
+    })
+    .catch(err => {
+      console.warn(err)
+      alert('Error in the process of getting data')
+    })
+  }, [])
+
+
+
+
+ const onChangeFromPrice = (e) =>{
+  console.log(e)
+  
+  const result = (e/ratesRef.current[fromCurrency])*ratesRef.current[toCurrency]
+  setFromPrice(e)
+  setToPrice(result.toFixed(3))
+ }
+ 
+
+ const onChangeToPrice = (e) =>{
+  console.log(e)
+  
+  const result = (ratesRef.current[fromCurrency]/ratesRef.current[toCurrency])*e
+  setFromPrice(result.toFixed(3))
+  setToPrice(e)
+ }
+     
+ 
+//  const onChangeFromCurrency = (e) =>{
+//   setFromCurrency(e)
+//   const result = (rates[e]/rates[toCurrency])*toPrice
+//   setFromPrice(result)
+//  }
+
+//  const onChangeToCurrency = (e) =>{
+//   setToCurrency(e)
+//   const result = (fromPrice/rates[fromCurrency])*rates[e]
+//   setToPrice(result)
+//  }
+       
+useEffect(() => {
+ onChangeFromPrice(fromPrice)
+}, [fromCurrency])
+
+useEffect(() => {
+  onChangeToPrice(toPrice)
+ }, [toCurrency])
+
+
+
   return (
     <div className="wrapper">
       
-      <ExchangeBlock fromCurrency={fromCurrency}/>
-      <ExchangeBlock/>
+      <ExchangeBlock 
+      changePrice={onChangeFromPrice}
+      price={fromPrice} 
+      currency={fromCurrency} 
+      onChangeCurrency={setFromCurrency}/>
+      <ExchangeBlock 
+      changePrice={onChangeToPrice} 
+      price={toPrice} 
+      currency={toCurrency } 
+      onChangeCurrency={setToCurrency }/>
 
     </div>
   );
--- src/components/ExchangeBlock.js
+++ src/components/ExchangeBlock.js
@@ -1,13 +1,17 @@
 import React from 'react'
 
-function ExchangeBlock({currency}) {
+function ExchangeBlock({currency, onChangeCurrency, price, changePrice}) {
     const list = ['UAH', 'USD', 'EUR', 'PLN']
+    console.log(currency)
   return (
     <div className='block'>
         <ul>
-        {list.map( el => <li key={el}  className='active'>{el}</li> )}
+        {list.map( el => <li 
+        onClick={() => onChangeCurrency(el)}
+        key={el}
+        className={currency === el? 'active': ''}>{el}</li> )}
         </ul>
-        <input type="number" />
+        <input value={price} onChange={(e) => changePrice(e.target.value)} type="number" />
         
     </div>
   )
